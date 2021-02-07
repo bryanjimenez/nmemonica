eeafcea9546cea31a619e7bfd5511424f12c98c8
Bryan <bryanjimenez6@gmail.com>
2021-02-06 21:54:33 -0500

    refactor verbs to be filtered from vocabulary data (#37)

--- src/reducers/verbsRed.js
+++ src/reducers/verbsRed.js
@@ -1,14 +1,16 @@
 import { GET_VERBS } from "../actions/verbsAct";
+import { GET_VOCABULARY } from "../actions/vocabularyAct";
 
 const DEFAULT_STATE = { value: [] };
 const DEFAULT_ACTION = {};
 
 const verbsReducer = (state = DEFAULT_STATE, action = DEFAULT_ACTION) => {
   switch (action.type) {
+    case GET_VOCABULARY:
     case GET_VERBS:
       return {
         ...state,
-        value: action.value,
+        value: Object.values(action.value).filter((o) => o.grp === "verb"),
       };
     default:
       return state;
