4c30477fc1e752ea56531a81b5aa7c40b5a296d7
Bryan Jimenez <bryanjimenez6@gmail.com>
2021-02-07 22:17:13 -0500

    basic serviceworker func test

---
+++ test/functional/serviceworker.js
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+
+function deleteAllCaches(){
+    browser.url("https://bryanjimenez.github.io/nmemonica-demo");
+
+    const keysBefore = browser.execute(() => {
+        return caches.keys().then(k=>Promise.all(k.map(k=>caches.delete(k)))).then(()=>caches.keys())
+    });
+
+    // console.log(keysBefore)
+    expect(keysBefore).to.deep.equal([]);
+}
+
+
+describe("cache", function () {
+  it("creates caches after a visit", function () {
+
+    deleteAllCaches();
+
+    browser.url("https://bryanjimenez.github.io/nmemonica-demo");
+
+    browser.$("#page-content").waitForExist();
+
+    const keys = browser.execute(() => {
+      return caches.keys();
+    });
+
+    expect(keys).to.deep.equal(["nmemonica-data", "nmemonica-static"]);
+  });
+});
