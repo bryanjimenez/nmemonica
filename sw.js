f9e4676752a97e60cd8a8e638cbfdb3f09d67946
Bryan Jimenez <bryanjimenez6@gmail.com>
2021-09-13 23:00:13 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const cacheFilesConst = ["0301fbe829087f4e8b91cde9bf9496c5.jpeg","1062f5e41ef989b5973a457e55770974.png","236.0cb615c6104aa0af46e1.css","236.7ef92152.js","35872f035bddb00bb6bed6802ee78d72.png","388582fe2fdbf34450b199396860911c.png","edb1f64724de9f6f175c1efab91a9473.png","favicon.ico","fb3f97e84cbbbf0c3fdedec024222e88.png","icon192.png","icon512.png","index.html","main.0dd2d043.js","main.bd7446e3c47dc53a564d.css","manifest.webmanifest","maskable512.png","npm.babel.c5e8247e.js","npm.bootstrap.ac1e9f22059b5529bc61.css","npm.classnames.8f12f1d7.js","npm.clsx.6e5cca71.js","npm.css-vendor.4df2fdfe.js","npm.firebase.c44ecf6b.js","npm.fortawesome.6919438d.js","npm.history.e2ef1c87.js","npm.hoist-non-react-statics.00a88bd9.js","npm.hyphenate-style-name.0055c82f.js","npm.is-in-browser.3a68dd2c.js","npm.isarray.b99faedf.js","npm.jss-plugin-camel-case.271794fc.js","npm.jss-plugin-default-unit.d2fb9396.js","npm.jss-plugin-global.36a61ec9.js","npm.jss-plugin-nested.27ee2039.js","npm.jss-plugin-props-sort.f9c7060e.js","npm.jss-plugin-rule-value-function.c8aeda87.js","npm.jss-plugin-vendor-prefixer.6f58513f.js","npm.jss.eab36002.js","npm.lodash.16180d03.js","npm.material-ui.29fcee2f.js","npm.mini-create-react-context.cd39d446.js","npm.object-assign.43cf34ba.js","npm.path-to-regexp.3c245515.js","npm.primer.d3adb01c.js","npm.prop-types.5a8543b5.js","npm.react-dom.bf3dcfe3.js","npm.react-redux.c8f4b3d9.js","npm.react-router-dom.43e290bb.js","npm.react-router.7a8be827.js","npm.react-transition-group.9c9f1895.js","npm.react.0ff3225b.js","npm.redux-thunk.571a5839.js","npm.redux.57848e49.js","npm.resolve-pathname.05213e20.js","npm.scheduler.d7588745.js","npm.tiny-invariant.fe2a2a3b.js","npm.tslib.4e3f6e7b.js","runtime.c965b000.js"];
+const cacheFilesConst = ["0301fbe829087f4e8b91cde9bf9496c5.jpeg","1062f5e41ef989b5973a457e55770974.png","236.0cb615c6104aa0af46e1.css","236.7ef92152.js","35872f035bddb00bb6bed6802ee78d72.png","388582fe2fdbf34450b199396860911c.png","edb1f64724de9f6f175c1efab91a9473.png","favicon.ico","fb3f97e84cbbbf0c3fdedec024222e88.png","icon192.png","icon512.png","index.html","main.bd7446e3c47dc53a564d.css","main.fe0ec2c8.js","manifest.webmanifest","maskable512.png","npm.babel.c5e8247e.js","npm.bootstrap.1176cc60d9b0614f08a8.css","npm.classnames.8f12f1d7.js","npm.clsx.6e5cca71.js","npm.css-vendor.4df2fdfe.js","npm.firebase.c44ecf6b.js","npm.fortawesome.6919438d.js","npm.history.e2ef1c87.js","npm.hoist-non-react-statics.00a88bd9.js","npm.hyphenate-style-name.0055c82f.js","npm.is-in-browser.3a68dd2c.js","npm.isarray.b99faedf.js","npm.jss-plugin-camel-case.271794fc.js","npm.jss-plugin-default-unit.d2fb9396.js","npm.jss-plugin-global.36a61ec9.js","npm.jss-plugin-nested.27ee2039.js","npm.jss-plugin-props-sort.f9c7060e.js","npm.jss-plugin-rule-value-function.c8aeda87.js","npm.jss-plugin-vendor-prefixer.6f58513f.js","npm.jss.eab36002.js","npm.lodash.16180d03.js","npm.material-ui.29fcee2f.js","npm.mini-create-react-context.cd39d446.js","npm.object-assign.43cf34ba.js","npm.path-to-regexp.3c245515.js","npm.primer.d3adb01c.js","npm.prop-types.5a8543b5.js","npm.react-dom.bf3dcfe3.js","npm.react-redux.c8f4b3d9.js","npm.react-router-dom.43e290bb.js","npm.react-router.7a8be827.js","npm.react-transition-group.9c9f1895.js","npm.react.0ff3225b.js","npm.redux-thunk.571a5839.js","npm.redux.57848e49.js","npm.resolve-pathname.05213e20.js","npm.scheduler.d7588745.js","npm.tiny-invariant.fe2a2a3b.js","npm.tslib.4e3f6e7b.js","runtime.c965b000.js"];
 
-const swVersionConst =  'a03e1b17537d61076b134f3e38869c38';
+const swVersionConst =  '240fbcb02e927f3c85314ee02666ec3f';
 
 const ghURLConst =  'https://bryanjimenez.github.io/nmemonica';
 const fbURLConst =  'https://nmemonica-9d977.firebaseio.com';
@@ -95,12 +95,156 @@ self.addEventListener("fetch", (e) => {
     e.respondWith(recache(appMediaCache, newUrl));
   } else if (url.indexOf(gCloudFnPronounce) === 0) {
     // site media asset
-    e.respondWith(appMediaReq(url));
+
+    console.log('site media asset')
+
+
+    if (!self.indexedDB) {
+      console.log(
+        "Your browser doesn't support a stable version of IndexedDB."
+      );
+    } else {
+      let db;
+      let openRequest = indexedDB.open(appMediaCache);
+      // db.onerror = function (event) {
+      //   // Generic error handler for all errors targeted at this database's
+      //   // requests!
+      //   console.error("Database error: " + event.target.errorCode);
+      // };
+
+      openRequest.onupgradeneeded = function (event) {
+        // Save the IDBDatabase interface
+        let db = event.target.result;
+
+        // Create an objectStore for this database
+        let objectStore = db.createObjectStore("media", { keyPath: "url" });
+        objectStore.createIndex("url", "url", { unique: true });
+      };
+
+      const dbOpenPromise = new Promise((resolve, reject) => {
+        openRequest.onerror = function (event) {
+          console.log("Why didn't you allow my web app to use IndexedDB?!");
+          reject();
+        };
+        openRequest.onsuccess = function (event) {
+          db = event.target.result;
+          console.log("success opening");
+          resolve(db);
+        };
+      });
+
+      const dbResults = dbOpenPromise.then((db) =>{
+        return readIndexedDB(db, url)
+          .then((obj) => {
+            //found
+            return obj.response;
+          })
+          .catch(() => {
+            //not found
+            return fetch(url).then((res) =>
+              writeIndexedDB(db, { url, response: res }).then((obj) => {
+                return obj.response;
+              })
+            );
+          })
+        });
+
+      e.respondWith(dbResults);
+    }
+
+    // e.respondWith(appMediaReq(url));
   } else {
     // everything else
     e.respondWith(fetch(e.request));
   }
 });
+
+function testfunc(url) {
+return 2
+}
+
+function readIndexedDB(db, key) {
+  var transaction = db.transaction(["media"]);
+  var objectStore = transaction.objectStore("media");
+  var request = objectStore.get(key);
+
+  const readPromise = new Promise((resolve, reject) => {
+    request.onerror = function (event) {
+      // Handle errors!
+      console.log("get fail");
+      reject();
+    };
+    request.onsuccess = function (event) {
+      // Do something with the request.result!
+      // console.log("Name for SSN 444-44-4444 is " + request.result.name);
+      console.log("get success");
+      resolve(request.result);
+    };
+  });
+
+  const transPromise = new Promise((resolve, reject) => {
+    transaction.oncomplete = function (event) {
+      console.log("Transaction done!");
+      // resolve();
+    };
+    transaction.onerror = function (event) {
+      console.log("Transaction failed!");
+      // reject();
+    };
+  });
+
+  // FIXME
+  return readPromise;
+}
+
+/**
+ *
+ * @param {*} db
+ * @param {*} value
+ * @returns
+ */
+function writeIndexedDB(db, value) {
+  let transaction = db.transaction(["media"], "readwrite");
+
+  let objectStore = transaction.objectStore("media");
+  let addRequest = objectStore.add(value);
+
+  const writePromise = new Promise((resolve, reject) => {
+    addRequest.onsuccess = function (event) {
+      // event.target.result === customer.ssn;
+      console.log("Add done!");
+      // resolve();
+    };
+    addRequest.onerror = function (event) {
+      console.log("Transaction failed!");
+      // reject();
+    };
+  });
+
+  const transPromise = new Promise((resolve, reject) => {
+    transaction.oncomplete = function (event) {
+      console.log("Transaction done!");
+      resolve(value);
+    };
+    transaction.onerror = function (event) {
+      console.log("Transaction failed!");
+      reject();
+    };
+  });
+
+  return transPromise;
+}
+
+function deleteIDB(db, item) {
+  var request = db
+    .transaction(["media"], "readwrite")
+    .objectStore("media")
+    .delete(item);
+  request.onsuccess = function (event) {
+    // It's gone!
+    console.log("deleted");
+  };
+}
 
 /**
  * respond with cache match always fetch and re-cache
@@ -108,7 +252,6 @@ self.addEventListener("fetch", (e) => {
  * @returns a Promise with a cache response
  */
 function appVersionReq() {
-
   // return what's on cache
   const cacheRes = caches
     .open(appDataCache)
