ee261a823551e96d4cd6f5d912f6640e1bc5fd13
Bryan Jimenez <bryanjimenez6@gmail.com>
2021-09-16 09:34:46 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const cacheFilesConst = ["0301fbe829087f4e8b91cde9bf9496c5.jpeg","1062f5e41ef989b5973a457e55770974.png","236.0cb615c6104aa0af46e1.css","236.7ef92152.js","35872f035bddb00bb6bed6802ee78d72.png","388582fe2fdbf34450b199396860911c.png","edb1f64724de9f6f175c1efab91a9473.png","favicon.ico","fb3f97e84cbbbf0c3fdedec024222e88.png","icon192.png","icon512.png","index.html","main.bd7446e3c47dc53a564d.css","main.c542f385.js","manifest.webmanifest","maskable512.png","npm.babel.c5e8247e.js","npm.bootstrap.1176cc60d9b0614f08a8.css","npm.classnames.8f12f1d7.js","npm.clsx.6e5cca71.js","npm.css-vendor.4df2fdfe.js","npm.firebase.c44ecf6b.js","npm.fortawesome.6919438d.js","npm.history.e2ef1c87.js","npm.hoist-non-react-statics.00a88bd9.js","npm.hyphenate-style-name.0055c82f.js","npm.is-in-browser.3a68dd2c.js","npm.isarray.b99faedf.js","npm.jss-plugin-camel-case.271794fc.js","npm.jss-plugin-default-unit.d2fb9396.js","npm.jss-plugin-global.36a61ec9.js","npm.jss-plugin-nested.27ee2039.js","npm.jss-plugin-props-sort.f9c7060e.js","npm.jss-plugin-rule-value-function.c8aeda87.js","npm.jss-plugin-vendor-prefixer.6f58513f.js","npm.jss.eab36002.js","npm.lodash.16180d03.js","npm.material-ui.29fcee2f.js","npm.mini-create-react-context.cd39d446.js","npm.object-assign.43cf34ba.js","npm.path-to-regexp.3c245515.js","npm.primer.d3adb01c.js","npm.prop-types.5a8543b5.js","npm.react-dom.bf3dcfe3.js","npm.react-redux.c8f4b3d9.js","npm.react-router-dom.43e290bb.js","npm.react-router.7a8be827.js","npm.react-transition-group.9c9f1895.js","npm.react.0ff3225b.js","npm.redux-thunk.571a5839.js","npm.redux.57848e49.js","npm.resolve-pathname.05213e20.js","npm.scheduler.d7588745.js","npm.tiny-invariant.fe2a2a3b.js","npm.tslib.4e3f6e7b.js","runtime.c965b000.js"];
+const cacheFilesConst = ["0301fbe829087f4e8b91cde9bf9496c5.jpeg","1062f5e41ef989b5973a457e55770974.png","236.0cb615c6104aa0af46e1.css","236.7ef92152.js","35872f035bddb00bb6bed6802ee78d72.png","388582fe2fdbf34450b199396860911c.png","edb1f64724de9f6f175c1efab91a9473.png","favicon.ico","fb3f97e84cbbbf0c3fdedec024222e88.png","icon192.png","icon512.png","index.html","main.0dd2d043.js","main.bd7446e3c47dc53a564d.css","manifest.webmanifest","maskable512.png","npm.babel.c5e8247e.js","npm.bootstrap.ac1e9f22059b5529bc61.css","npm.classnames.8f12f1d7.js","npm.clsx.6e5cca71.js","npm.css-vendor.4df2fdfe.js","npm.firebase.c44ecf6b.js","npm.fortawesome.6919438d.js","npm.history.e2ef1c87.js","npm.hoist-non-react-statics.00a88bd9.js","npm.hyphenate-style-name.0055c82f.js","npm.is-in-browser.3a68dd2c.js","npm.isarray.b99faedf.js","npm.jss-plugin-camel-case.271794fc.js","npm.jss-plugin-default-unit.d2fb9396.js","npm.jss-plugin-global.36a61ec9.js","npm.jss-plugin-nested.27ee2039.js","npm.jss-plugin-props-sort.f9c7060e.js","npm.jss-plugin-rule-value-function.c8aeda87.js","npm.jss-plugin-vendor-prefixer.6f58513f.js","npm.jss.eab36002.js","npm.lodash.16180d03.js","npm.material-ui.29fcee2f.js","npm.mini-create-react-context.cd39d446.js","npm.object-assign.43cf34ba.js","npm.path-to-regexp.3c245515.js","npm.primer.d3adb01c.js","npm.prop-types.5a8543b5.js","npm.react-dom.bf3dcfe3.js","npm.react-redux.c8f4b3d9.js","npm.react-router-dom.43e290bb.js","npm.react-router.7a8be827.js","npm.react-transition-group.9c9f1895.js","npm.react.0ff3225b.js","npm.redux-thunk.571a5839.js","npm.redux.57848e49.js","npm.resolve-pathname.05213e20.js","npm.scheduler.d7588745.js","npm.tiny-invariant.fe2a2a3b.js","npm.tslib.4e3f6e7b.js","runtime.c965b000.js"];
 
-const swVersionConst =  '099865309d659b9421a78eeb724bdee1';
+const swVersionConst =  'a03e1b17537d61076b134f3e38869c38';
 
 const ghURLConst =  'https://bryanjimenez.github.io/nmemonica';
 const fbURLConst =  'https://nmemonica-9d977.firebaseio.com';
@@ -92,255 +92,15 @@ self.addEventListener("fetch", (e) => {
     // override cache site media asset
     console.log("[ServiceWorker] Overriding Asset in Cache");
     const newUrl = url.split("/override_cache").join("");
-    if (!self.indexedDB) {
-      // use cache
-      console.log(
-        "Your browser doesn't support a stable version of IndexedDB."
-      );
-      e.respondWith(recache(appMediaCache, newUrl));
-    } else {
-      // use indexedDB
-
-      // TODO: parallel?
-      const dbOpenPromise = openIDB();
-      const dbResults = dbOpenPromise.then((db) => {
-        const query = newUrl.split(gCloudFnPronounce)[1];
-
-        return fetch(newUrl)
-          .then((res) => res.blob())
-          .then((blob) =>
-            putIDBItem(db, { query, blob }).then((dataO) => toResponse(dataO))
-          );
-      });
-
-      e.respondWith(dbResults);
-    }
+    e.respondWith(recache(appMediaCache, newUrl));
   } else if (url.indexOf(gCloudFnPronounce) === 0) {
     // site media asset
-    if (!self.indexedDB) {
-      // use cache
-      console.log(
-        "Your browser doesn't support a stable version of IndexedDB."
-      );
-      e.respondWith(appMediaReq(url));
-    } else {
-      // use indexedDB
-      const dbOpenPromise = openIDB();
-
-      const dbResults = dbOpenPromise.then((db) => {
-        const query = url.split(gCloudFnPronounce)[1];
-
-        return getIDBItem(db, query)
-          .then((dataO) =>
-            //found
-            toResponse(dataO)
-          )
-          .catch(() =>
-            //not found
-            fetch(url)
-              .then((res) => res.blob())
-              .then((blob) =>
-                addIDBItem(db, { query, blob }).then((dataO) =>
-                  toResponse(dataO)
-                )
-              )
-          );
-      });
-      e.respondWith(dbResults);
-    }
+    e.respondWith(appMediaReq(url));
   } else {
     // everything else
     e.respondWith(fetch(e.request));
   }
 });
-
-function toResponse(obj) {
-  const status = 200,
-    statusText = "OK";
-  const init = { status, statusText };
-  return new Response(obj.blob, init);
-}
-
-function openIDB() {
-  let openRequest = indexedDB.open(appMediaCache);
-
-  const upgradeP = new Promise((resolve, reject) => {
-    openRequest.onupgradeneeded = function (event) {
-      // Save the IDBDatabase interface
-      let db = event.target.result;
-
-      // Create an objectStore for this database
-      let objectStore = db.createObjectStore("media", { keyPath: "query" });
-      objectStore.createIndex("query", "query", { unique: true });
-
-      // Use transaction oncomplete to make sure the objectStore creation is
-      // finished before adding data into it.
-      objectStore.transaction.oncomplete = function (event) {
-        // Store values in the newly created objectStore.
-        // console.log("upgrade success");
-        resolve(db);
-      };
-    };
-  });
-
-  const dbOpenPromise = new Promise((resolve, reject) => {
-    openRequest.onerror = function (event) {
-      // console.log("open failed");
-      reject();
-    };
-    openRequest.onsuccess = function (event) {
-      let db = event.target.result;
-
-      db.onerror = function (event) {
-        // Generic error handler for all errors targeted at this database's
-        // requests!
-        console.error("Database error: " + event.target.errorCode);
-      };
-
-      // console.log("open success");
-      resolve(db);
-    };
-  });
-
-  // TODO: upgrade and open
-  return dbOpenPromise;
-}
-
-function getIDBItem(db, key) {
-  var transaction = db.transaction(["media"]);
-  var objectStore = transaction.objectStore("media");
-  var request = objectStore.get(key);
-
-  const requestP = new Promise((resolve, reject) => {
-    request.onerror = function (event) {
-      // Handle errors!
-      // console.log("read fail");
-      reject();
-    };
-    request.onsuccess = function (event) {
-      // console.log(JSON.stringify(request.result));
-      if (request.result) {
-        // console.log("read success");
-        resolve(request.result);
-      } else {
-        // console.log("no data?");
-        reject();
-      }
-    };
-  });
-
-  const transactionP = new Promise((resolve, reject) => {
-    transaction.oncomplete = function (event) {
-      // console.log("read Transaction done!");
-      resolve();
-    };
-    transaction.onerror = function (event) {
-      // console.log("read Transaction failed!");
-      reject();
-    };
-  });
-
-  return Promise.all([requestP, transactionP]).then((pArr) => pArr[0]);
-}
-
-/**
- *
- * @param {*} db
- * @param {*} value
- * @returns
- */
-function addIDBItem(db, value) {
-  let transaction = db.transaction(["media"], "readwrite");
-
-  let objectStore = transaction.objectStore("media");
-  let request = objectStore.add(value);
-
-  const requestP = new Promise((resolve, reject) => {
-    request.onsuccess = function (event) {
-      // event.target.result === customer.ssn;
-      // console.log("write done!");
-      resolve();
-    };
-    request.onerror = function (event) {
-      // console.log("write failed!");
-      reject();
-    };
-  });
-
-  const transactionP = new Promise((resolve, reject) => {
-    transaction.oncomplete = function (event) {
-      // console.log("write Transaction done!");
-      resolve(value);
-    };
-    transaction.onerror = function (event) {
-      // console.log("write Transaction failed!");
-      reject();
-    };
-  });
-
-  return Promise.all([requestP, transactionP]).then(() => value);
-}
-
-/**
- *
- * @param {*} db
- * @param {*} value
- * @returns
- */
-function putIDBItem(db, value) {
-  let transaction = db.transaction(["media"], "readwrite");
-
-  let objectStore = transaction.objectStore("media");
-  let request = objectStore.put(value);
-
-  const requestP = new Promise((resolve, reject) => {
-    request.onsuccess = function (event) {
-      // event.target.result === customer.ssn;
-      // console.log("put done!");
-      resolve();
-    };
-    request.onerror = function (event) {
-      // console.log("put failed!");
-      reject();
-    };
-  });
-
-  const transactionP = new Promise((resolve, reject) => {
-    transaction.oncomplete = function (event) {
-      // console.log("put Transaction done!");
-      resolve(value);
-    };
-    transaction.onerror = function (event) {
-      // console.log("put Transaction failed!");
-      reject();
-    };
-  });
-
-  return Promise.all([requestP, transactionP]).then(() => value);
-}
-
-function deleteIDBItem(db, key) {
-  var transaction = db.transaction(["media"], "readwrite");
-
-  let request = transaction.objectStore("media").delete(key);
-
-  request.onsuccess = function (event) {
-    // console.log("deleted success");
-  };
-
-  const transactionP = new Promise((resolve, reject) => {
-    transaction.oncomplete = function (event) {
-      // console.log("delete Transaction done!");
-      resolve();
-    };
-    transaction.onerror = function (event) {
-      // console.log("delete Transaction failed!");
-      reject();
-    };
-  });
-
-  return transactionP;
-}
 
 /**
  * respond with cache match always fetch and re-cache
@@ -348,6 +108,7 @@ function deleteIDBItem(db, key) {
  * @returns a Promise with a cache response
  */
 function appVersionReq() {
+
   // return what's on cache
   const cacheRes = caches
     .open(appDataCache)
