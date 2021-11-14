ea6d91566692e72d8ce79f33d17d4710170a866a
Bryan Jimenez <bryanjimenez6@gmail.com>
2021-11-13 20:10:55 -0500

    remove unused dep

--- webpack.config.js
+++ webpack.config.js
@@ -92,12 +92,14 @@ module.exports = function (webpackEnv, argv) {
             name(module) {
               // get the name. E.g. node_modules/packageName/not/this/part.js
               // or node_modules/packageName
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1];
+              if(module.context.indexOf('node_modules') > -1){
+                const packageName = module.context.match(
+                  /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                )[1];
 
-              // npm package names are URL-safe, but some servers don't like @ symbols
-              return `npm.${packageName.replace("@", "")}`;
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace("@", "")}`;
+              }
             },
           },
         },
