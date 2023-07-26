3fbd8382af932786d0721d4e74eb34a2216a8e12
bryanjimenez <bryanjimenez6@gmail.com>
2023-07-26 14:40:54 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const buildConstants = {
   swVersion: "5bad10eb",
-  initCacheVer: "5efbeaaf",
+  initCacheVer: "01ffe8de",
   SERVICE_WORKER_LOGGER_MSG: "service_worker_logger_msg",
   SERVICE_WORKER_NEW_TERMS_ADDED: "service_worker_new_terms",
   authenticationHeader: "X-API-KEY",
@@ -824,7 +824,7 @@ const cacheFiles = [
   "icon512.png",
   "index.html",
   "main.5d7d1cf299bac9471ffb.css",
-  "main.92240127.js",
+  "main.a9564500.js",
   "manifest.webmanifest",
   "maskable512.png",
   "npm.babel.40c54934.js",
