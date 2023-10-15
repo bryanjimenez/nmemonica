8ef2b40b9d1edc218b814b5d9b03fe6070bc2c28
bryanjimenez <bryanjimenez6@gmail.com>
2023-10-15 18:26:32 -0400

    move SettingsStale & SettingsFailedFurigana

--- src/components/Form/SettingsFailedFurigana.tsx
+++ src/components/Form/SettingsFailedFurigana.tsx
@@ -1,14 +1,28 @@
-import { Suspense, useMemo } from "react";
+import { Suspense, useEffect, useMemo } from "react";
+import { useDispatch } from "react-redux";
 
 import { NotReady } from "./NotReady";
 import { JapaneseText, furiganaParseRetry } from "../../helper/JapaneseText";
 import { useConnectPhrase } from "../../hooks/useConnectPhrase";
 import { useConnectVocabulary } from "../../hooks/useConnectVocabulary";
+import { type AppDispatch } from "../../slices";
+import { getPhrase } from "../../slices/phraseSlice";
+import { getVocabulary } from "../../slices/vocabularySlice";
 import type { RawVocabulary } from "../../typings/raw";
 
 export default function SettingsFailedFurigana() {
+  const dispatch = useDispatch<AppDispatch>();
   const { vocabList: vocabulary } = useConnectVocabulary();
   const { phraseList: phrases } = useConnectPhrase();
+
+  useEffect(() => {
+    if (vocabulary.length === 0) {
+      void dispatch(getVocabulary());
+    }
+    if (phrases.length === 0) {
+      void dispatch(getPhrase());
+    }
+  }, []);
 
   const failedFurigana = useMemo(
     () => failedFuriganaList([...phrases, ...vocabulary]),
