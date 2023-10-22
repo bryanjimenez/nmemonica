7092c86e68b756236e137d81c808956806fb7b7f
bryanjimenez <bryanjimenez6@gmail.com>
2023-10-22 12:54:55 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const buildConstants = {
   swVersion: "5bad10eb",
-  initCacheVer: "9b918e49",
+  initCacheVer: "b8311066",
   SERVICE_WORKER_LOGGER_MSG: "service_worker_logger_msg",
   SERVICE_WORKER_NEW_TERMS_ADDED: "service_worker_new_terms",
   authenticationHeader: "X-API-KEY",
@@ -821,7 +821,7 @@ const cacheFiles = [
   "424.58c15394.js",
   "457.6d27ea9c.js",
   "52.c79abb38.js",
-  "546.80028d31.js",
+  "546.6cc6e065.js",
   "557.ad5381ec.js",
   "637.67449247.js",
   "731.03b44a10.js",
@@ -833,8 +833,8 @@ const cacheFiles = [
   "icon192.png",
   "icon512.png",
   "index.html",
+  "main.1abe1ab3.js",
   "main.2cb7f9d89798932fab5b.css",
-  "main.ce17c277.js",
   "manifest.webmanifest",
   "maskable512.png",
   "npm.babel.40c54934.js",
@@ -863,7 +863,7 @@ const cacheFiles = [
   "npm.scheduler.7afb3de9.js",
   "npm.stylis.cd8656fe.js",
   "npm.use-sync-external-store.5abdc3a4.js",
-  "runtime.73bf273b.js",
+  "runtime.1ec56456.js",
 ];
 
 initServiceWorker({ ...buildConstants, getParam, removeParam, cacheFiles });
