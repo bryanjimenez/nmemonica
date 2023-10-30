40d1ff70aca18c31626e88f6452d20eec600c21d
bryanjimenez <bryanjimenez6@gmail.com>
2023-10-30 17:37:09 -0400

    use rspack

---
+++ environment-dep-replace.cjs
@@ -0,0 +1,40 @@
+// https://rspack.org/api/loader-api.html
+/**
+ * When in PRODUCTION
+ * Replace all source imports from .development to .production
+ * @param {string} content file contents
+ */
+module.exports = function productionDependencyReplacement(content, map, meta) {
+  if (
+    this.resourcePath.includes("node_modules") ||
+    this.mode !== "production"
+  ) {
+    return content;
+  }
+
+  // console.log("context: "+JSON.stringify(this.context))
+  // console.log("options: "+JSON.stringify(this.getOptions()))
+  // console.log("mode: "+JSON.stringify(this.mode))
+  // console.log("path: "+JSON.stringify(this.resourcePath));
+  // console.log("reso: "+JSON.stringify(this.resource));
+
+  const hasDevDep = /[\/\.\w]+?(?=\.development)/g;
+  const match = [...content.matchAll(hasDevDep)];
+
+  // console.log(JSON.stringify(match))
+  if (match.length > 0) {
+    console.log(`\npath: ${JSON.stringify(this.resourcePath)}`);
+    const prodContent = match.reduce((acc, m) => {
+      const [matchText] = m;
+      const dep = `${matchText}.development`;
+      console.log(`dep: ${dep}`);
+
+      return acc.replace(dep, `${matchText}.production`);
+    }, content);
+
+    // console.log(`\n\n${c.slice(0,700)}`);
+    return prodContent;
+  }
+
+  return content;
+};
---
+++ rspack.config.cjs
@@ -0,0 +1,70 @@
+// import path from "path";
+const path = require("path");
+// import { fileURLToPath } from "url";
+// const fileURLToPath = require("url").fileURLToPath;
+
+// https://www.rspack.dev/config/devtool.html
+// https://www.rspack.dev/guide/migrate-from-webpack.html
+
+// mimic CommonJS variables -- not needed if using CommonJS
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+module.exports = function (env, argv) {
+  const isProduction = process.env.NODE_ENV === "production";
+
+  return {
+    entry: {
+      main: "./src/index.tsx",
+    },
+    output: {
+      filename: "[name].[chunkhash:8].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+
+    devtool: isProduction
+      ? "cheap-module-source-map"
+      : "eval-cheap-module-source-map",
+
+    builtins: {
+      html: [{ template: `index${isProduction ? ".production" : ""}.html` }],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          type: "css", // this is enabled by default for .css, so you don't need to specify it
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/i,
+          type: "asset/resource",
+        },
+        ...(isProduction
+          ? [
+              {
+                test: /\.(jsx?|tsx?)$/i,
+                // loader: require.resolve('./normal-module-replacement.cjs'),
+                use: (info) => ({
+                  loader: require.resolve("./environment-dep-replace.cjs"),
+                  options: {
+                    loaderOptionParam: "paramValue",
+                  },
+                }),
+              },
+            ]
+          : [/** in dev don't replace dependencies */]),
+      ],
+    },
+
+    optimization: {
+      chunkIds: "deterministic",
+    },
+
+    devServer: {
+      port: process.env.PORT || 8080, // Port Number
+      host: "localhost", // Change to '0.0.0.0' for external facing server
+      static: [{ directory: path.resolve(__dirname, "dist") }],
+    },
+  };
+};
