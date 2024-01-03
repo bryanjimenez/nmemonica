d5854a73b96482b859901f2fac686f9dfd87e5c8
bryanjimenez <bryanjimenez6@gmail.com>
2024-01-03 16:50:48 -0500

    fix: Kanji useWindowSize init dims

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -3,18 +3,34 @@ import React, { useEffect, useState } from "react";
 import { getWindow } from "../helper/browserGlobal";
 
 /**
+ * Get initial screen size using Screen orientation API
+ * @link [MDN](https://developer.mozilla.org/en-US/docs/Web/API/Screen_Orientation_API)
+ * @returns
+ */
+// function initializeFromScreen() {
+//   if (screen.availWidth && screen.availHeight) {
+//     return { width: screen.availWidth, height: screen.availHeight };
+//   }
+
+//   return { width: undefined, height: undefined };
+// }
+
+function initializeFromWindow() {
+  if (window.innerWidth && window.innerHeight) {
+    return { width: window.innerWidth, height: window.innerHeight };
+  }
+
+  return { width: undefined, height: undefined };
+}
+
+/**
  * https://usehooks.com/useWindowSize/
  */
 export function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState<{
     width?: number;
     height?: number;
-  }>({
-    width: undefined,
-    height: undefined,
-  });
+  }>(initializeFromWindow);
   useEffect(() => {
     const w = getWindow();
     // Handler to call on window resize
