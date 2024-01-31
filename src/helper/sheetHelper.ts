f7c03c464d145578f8b8c9f02419f982bd4020f8
bryanjimenez <bryanjimenez6@gmail.com>
2024-01-30 22:56:00 -0500

    Sheet fetch in slices

---
+++ src/helper/sheetHelper.ts
@@ -0,0 +1,127 @@
+import { getLastCellIdx } from "@nmemonica/snservice/src/helper/sheetHelper";
+import type Spreadsheet from "@nmemonica/x-spreadsheet";
+import { SheetData } from "@nmemonica/x-spreadsheet";
+
+export function getActiveSheet(workbook: Spreadsheet) {
+  // TODO: fix SpreadSheet.getData type
+  const sheets = workbook.getData() as SheetData[];
+
+  const activeSheetName: string = workbook.bottombar.activeEl.el.innerHTML;
+  const activeSheetData =
+    sheets.find((sheet) => sheet.name === activeSheetName) ?? sheets[0];
+
+  const data = removeLastRowIfBlank(activeSheetData);
+
+  return { activeSheetName, activeSheetData: data };
+}
+
+export function removeLastRowIfBlank<T extends SheetData>(o: T) {
+  const rows = o.rows;
+  if (!o.rows || !rows) {
+    return o;
+  }
+
+  const clone = { ...o, rows };
+
+  const last = getLastCellIdx(o.rows);
+
+  if (
+    Object.values(o.rows[last].cells).every(
+      (c) => c.text === undefined || c.text.length === 0 || c.text.trim() === ""
+    )
+  ) {
+    delete clone.rows[last];
+    // @ts-expect-error SheetData.rows.len
+    clone.rows.len -= 1;
+  }
+
+  return clone;
+}
+
+export function addExtraRow(xObj: SheetData[]) {
+  const extraAdded = xObj.reduce<SheetData[]>((acc, o) => {
+    let rows = o.rows;
+    if (!rows) {
+      return [...acc, { rows: { "0": { cells: {} } }, len: 1 }];
+    }
+
+    const last = getLastCellIdx(rows);
+
+    const n = {
+      ...o,
+      rows: {
+        ...rows,
+        [String(last + 1)]: { cells: {} },
+        // @ts-expect-error SheetData.rows.len
+        len: rows.len + 1,
+      },
+    };
+
+    return [...acc, n];
+  }, []);
+
+  return extraAdded;
+}
+
+export function searchInSheet(sheet: SheetData, query: string) {
+  if (!sheet.rows) {
+    return [];
+  }
+
+  const result = Object.values(sheet.rows).reduce<[number, number, string][]>(
+    (acc, row, x) => {
+      if (typeof row !== "number" && "cells" in row) {
+        const find = Object.keys(row.cells).find((c) =>
+          row.cells[Number(c)].text?.toLowerCase().includes(query.toLowerCase())
+        );
+        if (find === undefined) return acc;
+
+        const text = row.cells[Number(find)].text;
+        if (text === undefined) return acc;
+
+        const y = Number(find);
+        acc = [...acc, [x, y, text]];
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  return result;
+}
+
+/**
+ * Check if device has touch screen
+ * [MDN mobile detection](https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent)
+ * @returns
+ */
+export function touchScreenCheck() {
+  let hasTouchScreen = false;
+
+  if (
+    "maxTouchPoints" in navigator &&
+    typeof navigator.maxTouchPoints === "number"
+  ) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if (
+    "msMaxTouchPoints" in navigator &&
+    typeof navigator.msMaxTouchPoints === "number"
+  ) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+    const mQ = matchMedia?.("(pointer:coarse)");
+    if (mQ?.media === "(pointer:coarse)") {
+      hasTouchScreen = Boolean(mQ.matches);
+    } else if ("orientation" in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = navigator.userAgent;
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+  return hasTouchScreen;
+}
--- src/hooks/useSubscribe.ts
+++ src/hooks/useSubscribe.ts
@@ -1,31 +1,9 @@
 import { MutableRefObject, useCallback } from "react";
 
-import {
-  pushServicePubKeyPath,
-  pushServiceRegisterClientPath,
-} from "../../environment.development";
 import { AppDispatch } from "../slices";
-import { logger } from "../slices/globalSlice";
-import { DebugLevel } from "../slices/settingHelper";
+import { registerSWForSubscription } from "../slices/sheetSlice";
 
-/**
- * Borrowed from MDN serviceworker cookbook
- * @link https://github.com/mdn/serviceworker-cookbook/blob/master/tools.js
- */
-export function urlBase64ToUint8Array(base64String: string) {
-  var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  var base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
-
-  var rawData = window.atob(base64);
-  var outputArray = new Uint8Array(rawData.length);
-
-  for (var i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
-export function useSubscribe(
+function useSubscribe(
   dispatch: AppDispatch,
   serviceAddress: MutableRefObject<string>
 ) {
@@ -35,40 +13,7 @@ export function useSubscribe(
 
     const serviceURL = serviceAddress.current;
 
-    void navigator.serviceWorker.ready.then((registration) => {
-      void registration.pushManager
-        .getSubscription()
-        .then(async (subscription) => {
-          if (subscription) {
-            return subscription;
-          }
-
-          const res = await fetch(serviceURL + pushServicePubKeyPath);
-          const keyBase64String = await res.text();
-          // Chrome doesn't accept the base64-encoded (string) vapidPublicKey yet
-          // urlBase64ToUint8Array() is defined in /tools.js
-          const keyUint8Array = urlBase64ToUint8Array(keyBase64String);
-
-          return registration.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: keyUint8Array,
-          });
-        })
-        .then((subscription) => {
-          void fetch(serviceURL + pushServiceRegisterClientPath, {
-            method: "post",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({
-              subscription,
-            }),
-          });
-        })
-        .catch((err: Error) => {
-          dispatch(logger("Push API: " + err.message, DebugLevel.ERROR));
-        });
-    });
+    registerSWForSubscription(dispatch, serviceURL);
   }, [dispatch, serviceAddress]);
 
   return {
