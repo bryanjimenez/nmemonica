d0d4605db28f850af3ee0ec5567ddc28967e6a91
bryanjimenez <bryanjimenez6@gmail.com>
2024-05-29 22:40:17 -0400

    verify lsSettings before allowing to share

--- src/components/Form/DataSetFromAppCache.tsx
+++ src/components/Form/DataSetFromAppCache.tsx
@@ -5,8 +5,11 @@ import {
   XIcon,
 } from "@primer/octicons-react";
 import classNames from "classnames";
+import { useMemo } from "react";
 
 import { TransferObject } from "./DataSetFromDragDrop";
+import { localStorageKey } from "../../constants/paths";
+import { getLocalStorageSettings } from "../../helper/localStorageHelper";
 import { metaDataNames, workbookSheetNames } from "../Pages/Sheet";
 
 interface DataSetFromAppCacheProps {
@@ -16,6 +19,11 @@ interface DataSetFromAppCacheProps {
 
 export function DataSetFromAppCache(props: DataSetFromAppCacheProps) {
   const { updateDataHandler, data } = props;
+
+  const hasUsrSettings = useMemo(() => {
+    const ls = getLocalStorageSettings(localStorageKey);
+    return ls instanceof Object;
+  }, []);
 
   return (
     <div className="text-center m-0 mb-1">
@@ -34,53 +42,71 @@ export function DataSetFromAppCache(props: DataSetFromAppCacheProps) {
             <span className="col px-2">Source</span>
           </div>
         </div>
-        {Object.values({ ...workbookSheetNames, ...metaDataNames }).map((el) => {
-          const { prettyName } = el;
-          const name = prettyName.toLowerCase();
+        {Object.values({ ...workbookSheetNames, ...metaDataNames }).map(
+          (el) => {
+            const { prettyName } = el;
+            const name = prettyName.toLowerCase();
 
-          const dataItem = data.find(
-            (d) => d.name.toLowerCase() === prettyName.toLowerCase()
-          );
+            const dataItem = data.find(
+              (d) => d.name.toLowerCase() === prettyName.toLowerCase()
+            );
 
-          return (
-            <div key={name} className="d-flex justify-content-between">
-              <div className="me-5">
-                <span
-                  className={classNames({
-                    "col fs-6": true,
-                    "opacity-25": dataItem?.name.toLowerCase() !== name,
-                  })}
-                >
-                  {prettyName}
-                </span>
-              </div>
-              <div>
-                <div className="row">
-                  <span className="col px-1">
-                    {dataItem?.sheet ? dataItem.sheet.rows.len : ""}
-                  </span>
-                  <div className="col px-1">
-                    {dataItem?.origin === "AppCache" && <DatabaseIcon />}
-                    {dataItem?.origin === "FileSystem" && <FileDirectoryIcon />}
-                  </div>
-
-                  <div
-                    className="col px-1 clickable"
-                    onClick={() => {
-                      updateDataHandler(prettyName);
-                    }}
+            return (
+              <div key={name} className="d-flex justify-content-between">
+                <div className="me-5">
+                  <span
+                    className={classNames({
+                      "col fs-6": true,
+                      "opacity-25": dataItem?.name.toLowerCase() !== name,
+                    })}
                   >
-                    {dataItem ? (
-                      <XIcon />
-                    ) : (
-                      <DiamondIcon className="rotate-45 px-0" />
-                    )}
+                    {prettyName}
+                  </span>
+                </div>
+                <div>
+                  <div className="row">
+                    <span className="col px-1">
+                      {dataItem?.sheet ? dataItem.sheet.rows.len : ""}
+                    </span>
+                    <div className="col px-1">
+                      {dataItem?.origin === "AppCache" && <DatabaseIcon />}
+                      {dataItem?.origin === "FileSystem" && (
+                        <FileDirectoryIcon />
+                      )}
+                    </div>
+
+                    <div
+                      className="col px-1 clickable"
+                      onClick={() => {
+                        if (
+                          name !==
+                            metaDataNames.settings.prettyName.toLowerCase() ||
+                          hasUsrSettings
+                        ) {
+                          updateDataHandler(prettyName);
+                        }
+                      }}
+                    >
+                      {dataItem ? (
+                        <XIcon />
+                      ) : (
+                        <DiamondIcon
+                          className={classNames({
+                            "rotate-45 px-0": true,
+                            "disabled opacity-25":
+                              name ===
+                                metaDataNames.settings.prettyName.toLowerCase() &&
+                              !hasUsrSettings,
+                          })}
+                        />
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          }
+        )}
       </div>
     </div>
   );
