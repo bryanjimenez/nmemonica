337bf483f72833155746a75a94bc397d4ea6c79c
Bryan <bryanjimenez6@gmail.com>
2024-06-07 21:40:09 -0400

    React slick license (#284)
    react slick license c7e9259
    add allowed licenses 028593b

--- rspack.config.js
+++ rspack.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "node:url";
 import LicenseCheckerWebpackPlugin from "license-checker-webpack-plugin";
 import { ca } from "@nmemonica/utils/signed-ca";
 import { config } from "@nmemonica/snservice";
+import { appendLicense } from "./license-writer.js";
 
 // https://www.rspack.dev/config/devtool.html
 // https://www.rspack.dev/guide/migrate-from-webpack.html
@@ -44,7 +45,14 @@ export default function rspackConfig(
       }),
 
       // output license info
-      ...(isProduction ? [new LicenseCheckerWebpackPlugin()] : []),
+      ...(isProduction
+        ? [
+            new LicenseCheckerWebpackPlugin({
+              allow: "MIT OR BSD-3-Clause OR CC-BY-4.0",
+              outputWriter: appendLicense,
+            }),
+          ]
+        : []),
       // index.html template
       new rspack.HtmlRspackPlugin({
         template: `index${isProduction ? ".production" : ""}.html`,
