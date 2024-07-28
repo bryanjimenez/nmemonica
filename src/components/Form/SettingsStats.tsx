aef69fede3f90cfa1b44aa60677bbcf3e82a5de9
bryanjimenez <bryanjimenez6@gmail.com>
2024-07-28 18:10:27 -0400

    move stat tables

--- src/components/Form/SettingsStats.tsx
+++ src/components/Form/SettingsStats.tsx
@@ -235,41 +235,49 @@ export default function SettingsStats() {
           <table className="w-50">
             <thead>
               <tr>
-                <th>Recall</th>
-                <td className="p-1">{"(0,1)"}</td>
-                <td className="p-1">{"[1,2)"}</td>
-                <td className="p-1">2.0</td>
-                <td className="p-1">-1</td>
+                <th className="pe-1">Recall</th>
+                <td className="pe-1">{"(0,1)"}</td>
+                <td className="pe-1">{"[1,2)"}</td>
+                <td className="pe-1">2.0</td>
+                <td className="pe-1">-1</td>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>Phrases:</td>
-                <td>{phraseR.pending}</td>
-                <td>{phraseR.due}</td>
-                <td>{phraseR.overdue}</td>
-                <td>{phraseR.wrong}</td>
+                <td className="pe-1">Phrases:</td>
+                <td className="pe-1">{phraseR.pending}</td>
+                <td className="pe-1">{phraseR.due}</td>
+                <td className="pe-1">{phraseR.overdue}</td>
+                <td className="pe-1">{phraseR.wrong}</td>
               </tr>
               <tr>
-                <td>Vocabulary:</td>
-                <td>{vocabR.pending}</td>
-                <td>{vocabR.due}</td>
-                <td>{vocabR.overdue}</td>
-                <td>{vocabR.wrong}</td>
+                <td className="pe-1">Vocabulary:</td>
+                <td className="pe-1">{vocabR.pending}</td>
+                <td className="pe-1">{vocabR.due}</td>
+                <td className="pe-1">{vocabR.overdue}</td>
+                <td className="pe-1">{vocabR.wrong}</td>
               </tr>
               <tr>
-                <td>Kanji:</td>
-                <td>{kanjiR.pending}</td>
-                <td>{kanjiR.due}</td>
-                <td>{kanjiR.overdue}</td>
-                <td>{kanjiR.wrong}</td>
+                <td className="pe-1">Kanji:</td>
+                <td className="pe-1">{kanjiR.pending}</td>
+                <td className="pe-1">{kanjiR.due}</td>
+                <td className="pe-1">{kanjiR.overdue}</td>
+                <td className="pe-1">{kanjiR.wrong}</td>
               </tr>
               <tr>
-                <td>{/** Totals */}</td>
-                <td>{phraseR.pending + vocabR.pending + kanjiR.pending}</td>
-                <td>{phraseR.due + vocabR.due + kanjiR.due}</td>
-                <td>{phraseR.overdue + vocabR.overdue + kanjiR.overdue}</td>
-                <td>{phraseR.wrong + vocabR.wrong + kanjiR.wrong}</td>
+                <td className="pe-1">{/** Totals */}</td>
+                <td className="pe-1">
+                  {phraseR.pending + vocabR.pending + kanjiR.pending}
+                </td>
+                <td className="pe-1">
+                  {phraseR.due + vocabR.due + kanjiR.due}
+                </td>
+                <td className="pe-1">
+                  {phraseR.overdue + vocabR.overdue + kanjiR.overdue}
+                </td>
+                <td className="pe-1">
+                  {phraseR.wrong + vocabR.wrong + kanjiR.wrong}
+                </td>
               </tr>
             </tbody>
           </table>
@@ -279,9 +287,12 @@ export default function SettingsStats() {
             <table className="w-50">
               <thead>
                 <tr>
-                  <th>Viewed</th>
+                  <th className="pe-1">Viewed</th>
                   {daysOW.map((dayOfTheWeek, i) => (
-                    <td key={`${i.toString()} ${dayOfTheWeek}`} className="p-1">
+                    <td
+                      key={`${i.toString()} ${dayOfTheWeek}`}
+                      className="pe-1"
+                    >
                       {i === 0 ? "Today" : dayOfTheWeek}
                     </td>
                   ))}
@@ -289,28 +300,37 @@ export default function SettingsStats() {
               </thead>
               <tbody>
                 <tr>
-                  <td>Phrases:</td>
+                  <td className="pe-1">Phrases:</td>
                   {phraseC.map((count, i) => (
-                    <td key={`${i.toString()} ${count}`}>{count}</td>
+                    <td className="pe-1" key={`${i.toString()} ${count}`}>
+                      {count}
+                    </td>
                   ))}
                 </tr>
                 <tr>
-                  <td>Vocabulary:</td>
+                  <td className="pe-1">Vocabulary:</td>
                   {vocabC.map((count, i) => (
-                    <td key={`${i.toString()} ${count}`}>{count}</td>
+                    <td className="pe-1" key={`${i.toString()} ${count}`}>
+                      {count}
+                    </td>
                   ))}
                 </tr>
                 <tr>
-                  <td>Kanji:</td>
+                  <td className="pe-1">Kanji:</td>
                   {kanjiC.map((count, i) => (
-                    <td key={`${i.toString()} ${count}`}>{count}</td>
+                    <td className="pe-1" key={`${i.toString()} ${count}`}>
+                      {count}
+                    </td>
                   ))}
                 </tr>
                 <tr>
-                  <td>{/** Totals */}</td>
+                  <td className="pe-1">{/** Totals */}</td>
                   {phraseC.map((el, i) => {
                     return (
-                      <td key={`total-${daysOW[i]}-${i.toString()}`}>
+                      <td
+                        className="pe-1"
+                        key={`total-${daysOW[i]}-${i.toString()}`}
+                      >
                         {(phraseC[i] ?? 0) +
                           (vocabC[i] ?? 0) +
                           (kanjiC[i] ?? 0)}
@@ -328,87 +348,58 @@ export default function SettingsStats() {
           <table className="w-50">
             <thead>
               <tr>
-                <th>lastView</th>
-                <td className="p-1">new</td>
-                <td className="p-1">mean</td>
-                <td className="p-1">q0</td>
-                <td className="p-1">q1</td>
-                <td className="p-1">q2</td>
-                <td className="p-1">q3</td>
-                <td className="p-1">q4</td>
+                <th className="pe-1">lastView</th>
+                <td className="pe-1">new</td>
+                <td className="pe-1">mean</td>
+                <td className="pe-1">q0</td>
+                <td className="pe-1">q1</td>
+                <td className="pe-1">q2</td>
+                <td className="pe-1">q3</td>
+                <td className="pe-1">q4</td>
               </tr>
             </thead>
             <tbody>
               {phraseQ.range > 0 && (
                 <tr>
-                  <td>Phrases:</td>
-                  <td>{phraseQ.unPlayed}</td>
-                  <td>{phraseQ.mean.toFixed(1)}</td>
-                  <td>{phraseQ.min}</td>
-                  <td>{phraseQ.q1}</td>
-                  <td>{phraseQ.q2}</td>
-                  <td>{phraseQ.q3}</td>
-                  <td>{phraseQ.max}</td>
+                  <td className="pe-1">Phrases:</td>
+                  <td className="pe-1">{phraseQ.unPlayed}</td>
+                  <td className="pe-1">{phraseQ.mean.toFixed(1)}</td>
+                  <td className="pe-1">{phraseQ.min}</td>
+                  <td className="pe-1">{phraseQ.q1}</td>
+                  <td className="pe-1">{phraseQ.q2}</td>
+                  <td className="pe-1">{phraseQ.q3}</td>
+                  <td className="pe-1">{phraseQ.max}</td>
                 </tr>
               )}
               {vocabQ.range > 0 && (
                 <tr>
-                  <td>Vocabulary:</td>
-                  <td>{vocabQ.unPlayed}</td>
-                  <td>{vocabQ.mean.toFixed(1)}</td>
-                  <td>{vocabQ.min}</td>
-                  <td>{vocabQ.q1}</td>
-                  <td>{vocabQ.q2}</td>
-                  <td>{vocabQ.q3}</td>
-                  <td>{vocabQ.max}</td>
+                  <td className="pe-1">Vocabulary:</td>
+                  <td className="pe-1">{vocabQ.unPlayed}</td>
+                  <td className="pe-1">{vocabQ.mean.toFixed(1)}</td>
+                  <td className="pe-1">{vocabQ.min}</td>
+                  <td className="pe-1">{vocabQ.q1}</td>
+                  <td className="pe-1">{vocabQ.q2}</td>
+                  <td className="pe-1">{vocabQ.q3}</td>
+                  <td className="pe-1">{vocabQ.max}</td>
                 </tr>
               )}
               {kanjiQ.range > 0 && (
                 <tr>
-                  <td>Kanji:</td>
-                  <td>{kanjiQ.unPlayed}</td>
-                  <td>{kanjiQ.mean.toFixed(1)}</td>
-                  <td>{kanjiQ.min}</td>
-                  <td>{kanjiQ.q1}</td>
-                  <td>{kanjiQ.q2}</td>
-                  <td>{kanjiQ.q3}</td>
-                  <td>{kanjiQ.max}</td>
+                  <td className="pe-1">Kanji:</td>
+                  <td className="pe-1">{kanjiQ.unPlayed}</td>
+                  <td className="pe-1">{kanjiQ.mean.toFixed(1)}</td>
+                  <td className="pe-1">{kanjiQ.min}</td>
+                  <td className="pe-1">{kanjiQ.q1}</td>
+                  <td className="pe-1">{kanjiQ.q2}</td>
+                  <td className="pe-1">{kanjiQ.q3}</td>
+                  <td className="pe-1">{kanjiQ.max}</td>
                 </tr>
               )}
             </tbody>
           </table>
         </div>
         <div className="column-2 setting-block">
-          <div className="mb-2">
-            <table className="w-50">
-              <thead>
-                <tr>
-                  <th>{/** Counts */}</th>
-                  <td>Terms</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Phrases:</td>
-                  <td>{phraseList.length}</td>
-                </tr>
-                <tr>
-                  <td>Vocabulary:</td>
-                  <td>{vocabList.length}</td>
-                </tr>
-                <tr>
-                  <td>Kanji:</td>
-                  <td>{kanjiList.length}</td>
-                </tr>
-                <tr>
-                  <td>{/** Totals */}</td>
-                  <td>
-                    {phraseList.length + vocabList.length + kanjiList.length}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <div className="mb-2"></div>
         </div>
       </div>
       <div className="d-flex flex-column flex-sm-row justify-content-between mb-2">
@@ -416,37 +407,76 @@ export default function SettingsStats() {
           <table className="w-50">
             <thead>
               <tr>
-                <th>difficultyP</th>
-                <td className="p-1">0</td>
-                <td className="p-1">11</td>
-                <td className="p-1">21</td>
-                <td className="p-1">31</td>
-                <td className="p-1">41</td>
-                <td className="p-1">51</td>
-                <td className="p-1">61</td>
-                <td className="p-1">71</td>
-                <td className="p-1">81</td>
-                <td className="p-1">91</td>
+                <th className="pe-1">difficultyP</th>
+                <td className="pe-1">0</td>
+                <td className="pe-1">11</td>
+                <td className="pe-1">21</td>
+                <td className="pe-1">31</td>
+                <td className="pe-1">41</td>
+                <td className="pe-1">51</td>
+                <td className="pe-1">61</td>
+                <td className="pe-1">71</td>
+                <td className="pe-1">81</td>
+                <td className="pe-1">91</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="pe-1">Phrases:</td>
+                {phraseD.map((count, i) => (
+                  <td className="pe-1" key={`${i.toString()} ${count}`}>
+                    {count}
+                  </td>
+                ))}
+              </tr>
+              <tr>
+                <td className="pe-1">Vocabulary:</td>
+                {vocabD.map((count, i) => (
+                  <td className="pe-1" key={`${i.toString()} ${count}`}>
+                    {count}
+                  </td>
+                ))}
+              </tr>
+              <tr>
+                <td className="pe-1">Kanji:</td>
+                {kanjiD.map((count, i) => (
+                  <td className="pe-1" key={`${i.toString()} ${count}`}>
+                    {count}
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="column-2 setting-block"></div>
+      </div>
+      <div className="d-flex flex-column flex-sm-row justify-content-between mb-2">
+        <div className="column-1 text-end">
+          <table className="w-50">
+            <thead>
+              <tr>
+                <th>{/** Counts */}</th>
+                <td>Terms</td>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>Phrases:</td>
-                {phraseD.map((count, i) => (
-                  <td key={`${i.toString()} ${count}`}>{count}</td>
-                ))}
+                <td>{phraseList.length}</td>
               </tr>
               <tr>
                 <td>Vocabulary:</td>
-                {vocabD.map((count, i) => (
-                  <td key={`${i.toString()} ${count}`}>{count}</td>
-                ))}
+                <td>{vocabList.length}</td>
               </tr>
               <tr>
                 <td>Kanji:</td>
-                {kanjiD.map((count, i) => (
-                  <td key={`${i.toString()} ${count}`}>{count}</td>
-                ))}
+                <td>{kanjiList.length}</td>
+              </tr>
+              <tr>
+                <td>{/** Totals */}</td>
+                <td>
+                  {phraseList.length + vocabList.length + kanjiList.length}
+                </td>
               </tr>
             </tbody>
           </table>
