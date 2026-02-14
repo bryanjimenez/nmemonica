2a8fa6dd980086d9f86644b88af9c306e91382a5
bryanjimenez <bryanjimenez6@gmail.com>
2026-02-14 15:10:00 -0500

    remove lighthouse + closes #315, closes #242

--- test/lighthouse/lighthouse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# https://github.com/GoogleChrome/lighthouse#using-the-node-cli
-# sudo npm install -g lighthouse
-
-# https://github.com/GoogleChrome/lighthouse#using-the-node-module
-lighthouse --chrome-flags="--no-sandbox --headless" --quiet --view https://bryanjimenez.github.io/nmemonica/
-
