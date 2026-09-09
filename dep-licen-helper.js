f392f69299ade7e7d1700c8fe4bc981583f1ec56
Bryan <bryanjimenez6@gmail.com>
2026-09-09 19:26:49 -0400

    Third party dependencies (#414)
    FIX: include license urls + remove unused + list sub projects fbe78b6
    DEP: voice-ja@0.1.13 31d8af6
    DEP: voice-en@0.0.1 3362971

--- dep-licen-helper.js
+++ dep-licen-helper.js
@@ -24,6 +24,30 @@ interface DependencyBlock {
     licenseText: string;
 }
 */
+
+const licenceLinks = {
+  MIT: "https://mit-license.org/",
+  "BSD-3-Clause": "https://opensource.org/license/bsd-3-clause",
+  "Apache-2.0": "https://www.apache.org/licenses/LICENSE-2.0",
+  "CC-BY-4.0": "https://creativecommons.org/licenses/by/4.0/",
+  ISC: "https://www.isc.org/licenses/",
+  Unlicense: "https://unlicense.org/",
+  "UBUNTU-FONT-1.0": "https://canonical.com/legal/font-licence",
+
+  // dual
+  "(CC-BY-4.0 AND MIT)":
+    "https://creativecommons.org/licenses/by/4.0/\nhttps://mit-license.org/",
+  "(MIT AND BSD-3-Clause)":
+    "https://mit-license.org/\nhttps://opensource.org/license/bsd-3-clause",
+  "Apache-2.0 OR MIT":
+    "https://www.apache.org/licenses/LICENSE-2.0\nhttps://mit-license.org/",
+
+  // special
+  "BSD-like": "", // not mentioned is required
+
+  "": "LICENSE: MISSING!!!",
+};
+
 /**
  * Append additional licenses
  * @param {{dependencies: DependencyBlock[]}} lic
@@ -38,42 +62,51 @@ export function appendLicense(lic /*:{dependencies:DependencyBlock[]}*/) {
     licenseText: fromComment("./src/helper/TouchSwipe.ts"),
   };
 
-  const hts_voice_nitech_jp_atr503_m001_1_05_lic = readFileSync(
-    "./res/models/hts_voice_nitech_jp_atr503_m001-1.05/COPYING",
-    { encoding: "utf-8" }
+  const ubuntuMono = {
+    name: "ubuntu-mono",
+    version: "17/KFOjCneDtsqEr0keqCMhbCc6CsQ",
+    author: "Dalton Maag",
+    repository:
+      "https://fonts.gstatic.com/s/ubuntumono/v17/KFOjCneDtsqEr0keqCMhbCc6CsQ.woff2",
+    licenseName: "UBUNTU-FONT-1.0",
+  };
+
+  const voiceJaLicenseBundle = readFileSync(
+    "./node_modules/@nmemonica/voice-ja/ThirdPartyNotice.json",
+    { encoding: "utf8" },
   );
-  const hts_voice_nitech_jp_atr503_m001_1_05 = {
-    name: "hts_voice_nitech_jp_atr503_m001-1.05",
-    version: "1.05",
-    author: "HTS Working Group",
-    repository: "http://open-jtalk.sourceforge.net/",
-    licenseName: "CC-BY-3.0",
-    licenseText: hts_voice_nitech_jp_atr503_m001_1_05_lic,
-  };
+  const voiceEnLicenseBundle = readFileSync(
+    "./node_modules/@nmemonica/voice-en/ThirdPartyNotice.json",
+    { encoding: "utf8" },
+  );
 
-  const tohoku_f01_lic = readFileSync("./res/models/tohoku-f01/COPYRIGHT.txt", {
-    encoding: "utf-8",
-  });
-  const htsvoice_tohoku_f01 = {
-    name: "htsvoice-tohoku-f01",
-    version: "8e33060",
-    author:
-      "2015 Intelligent Communication Network (Ito-Nose) Laboratory, Tohoku University",
-    repository: "https://github.com/icn-lab/htsvoice-tohoku-f01",
-    licenseName: "CC-BY-4.0",
-    licenseText: tohoku_f01_lic,
-  };
+  const {
+    duplicates: subProjectDuplicates,
+    dependencies: subprojectDependencies,
+  } = transformRustDepToNodeDep([
+    ...JSON.parse(voiceEnLicenseBundle),
+    ...JSON.parse(voiceJaLicenseBundle),
+  ]);
 
-  const appended = {
+  const projectLicenses = {
     dependencies: [
       ...lic.dependencies,
       reactSlickBlock,
-      hts_voice_nitech_jp_atr503_m001_1_05,
-      htsvoice_tohoku_f01,
+      ubuntuMono,
+      ...subprojectDependencies,
     ],
   };
 
-  return licenseJsonToString(appended);
+  console.log("Project Dependencies:");
+  displayTable(projectLicenses);
+
+  if (subProjectDuplicates.length > 0) {
+    const msg = "Duplicate dependencies found:";
+    console.log(`\x1b[1m\x1b[97m\x1b[101m${msg}\x1b[0m`);
+    displayTable({ dependencies: subProjectDuplicates });
+  }
+
+  return licenseJsonToString(projectLicenses);
 }
 
 /**
@@ -123,15 +156,80 @@ function licenseJsonToString(license /*:{dependencies:DependencyBlock[]}*/) {
     "Do Not Translate or Localize\n" +
     "\n";
   const s =
-    "--------------------------------------------------------------------------------\n";
-
+    "================================================================================\n";
   const text = license.dependencies.reduce((acc, dep) => {
-    const depLicenseChunk = `${s}${dep.name} v${dep.version} - ${dep.author}\n${
-      dep.repository
-    }\n${s}\n${dep.licenseText || dep.licenseName}\n`;
+    const { name, version, author, repository, licenseName, licenseText } = dep;
+    const licenceLink = licenceLinks[licenseName];
+
+    if (licenceLink === undefined) {
+      throw new Error(`Required link for ${name} ${licenseName}`);
+    }
+
+    const depLicenseChunk = `${s}- ${name} v${version} - ${author}\n- ${
+      repository
+    }\n${s}\n${licenseText ?? licenseName}\n${licenceLink}\n\n`;
 
     return acc + depLicenseChunk;
   }, "");
 
   return header + text;
+}
+
+/**
+ * Displays license info
+ * @param {{dependencies:DependencyBlock[]}} license
+ */
+function displayTable({ dependencies }) {
+  console.table(
+    dependencies
+      // .sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0))
+      .sort((a, b) =>
+        a.licenseName.length && b.licenseName.length > 0
+          ? a.licenseName.charCodeAt(0) - b.licenseName.charCodeAt(0)
+          : -1,
+      )
+      .map((l) => ({
+        name: l.name,
+        version: l.version,
+        license:
+          l.licenseName && l.licenseName.length > 0
+            ? l.licenseName
+            : "-------MISSING-------",
+        author:
+          l.author && l.author.length > 25
+            ? l.author?.slice(0, 25) + "..."
+            : l.author,
+      })),
+  );
+}
+
+/**
+ * Transform incoming dep object
+ * @param {DependencyBlock[]} arr
+ */
+function transformRustDepToNodeDep(arr) {
+  let duplicates = [];
+
+  const dependenciesl = Array.from(
+    arr
+      .reduce((acc, entry) => {
+        const depEntry = {
+          ...entry,
+          author: entry.authors,
+          licenseName: entry.license,
+        };
+
+        if (acc.has(entry.name)) {
+          duplicates = [...duplicates, depEntry];
+          return acc;
+        }
+
+        acc.set(entry.name, depEntry);
+
+        return acc;
+      }, new Map())
+      .values(),
+  );
+
+  return { duplicates, dependencies: dependenciesl };
 }
